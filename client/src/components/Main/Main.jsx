0ac08240259d802f0c77c01047f686470218a61a
rjn991 <ranjanht991@gmail.com>
2024-12-23 09:46:58 +0530

    Added Dialog

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -15,12 +15,32 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 const Main = () => {
+  const [addStudentName, addStudentNameUpdate] = useState();
+  const [addYear, addYearUpdate] = useState();
+  const [addClass, addClassUpdate] = useState();
+  const [courses, updateCourses] = useState();
+  const [addCourses, updateAddCourses] = useState();
+
   return (
     <div className="mt-3 mx-5 p-4 bg-white rounded-lg">
       <div className="flex place-content-between">
         <div className="flex gap-4">
+          {/* Left 2 Select items */}
           <Select>
             <SelectTrigger className="w-[150px]">
               <SelectValue placeholder="Select Year" />
@@ -40,10 +60,88 @@ const Main = () => {
             </SelectContent>
           </Select>
         </div>
+
+        {/* Add student button */}
         <div>
-          <Button>+ Add new Student</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>+ Add new Student</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Add new Student</DialogTitle>
+                <DialogDescription>
+                  Fill the details and click save.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Name
+                  </Label>
+                  <Input onChange={(e)=>{addStudentNameUpdate(e.target.value)}} required id="name" className="col-span-3" />
+                </div>
+
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="year" className="text-right">
+                    Year
+                  </Label>
+                  <Select
+                    className="col-span-3"
+                    id="yea"
+                    onValueChange={(e) => {
+                      addYearUpdate(e);
+                    }}
+                  >
+                    <SelectTrigger className="w-[150px]">
+                      <SelectValue placeholder="Select Year" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="ay2324">AY 2023 24</SelectItem>
+                      <SelectItem value="ay2425">AY 2024 25</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="year" className="text-right">
+                    Class
+                  </Label>
+                  <Select
+                    onValueChange={(e) => {
+                      addClassUpdate(e);
+                    }}
+                  >
+                    <SelectTrigger className="w-[140px]">
+                      <SelectValue placeholder="Select Class" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="8">8 CBSE</SelectItem>
+                      <SelectItem value="9">9 CBSE</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                
+              </div>
+              
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button
+                    type="submit"
+                    onClick={() => {
+                      console.log(addStudentName,addClass,addYear);
+                    }}
+                  >
+                    Save
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
+
+      {/* Table */}
       <Table>
         <TableHeader>
           <TableRow>
@@ -68,91 +166,104 @@ const Main = () => {
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
-        </TableBody><TableBody>
+        </TableBody>
+        <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
