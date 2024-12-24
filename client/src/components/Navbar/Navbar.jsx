8ede2ecefed15662738d6297caabbd837d97b7f2
rjn991 <ranjanht991@gmail.com>
2024-12-24 11:03:04 +0530

    Added Sidebar UI

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,25 @@ import altSettingsIcon from "../../assets/icons/settings-alt-icon.svg";
 import notificationIcon from "../../assets/icons/notification-icon.svg";
 import logo from "../../assets/logos/logo.svg";
 import sidebar from "../../assets/icons/sidebar.svg";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+
+
+import dashboardIcon from "../../assets/icons/dashboard-icon.svg";
+import studentsIcon from "../../assets/icons/students-icon.svg";
+import chapterIcon from "../../assets/icons/chapter-icon.svg";
+import reportsIcon from "../../assets/icons/reports-icon.svg";
+import settingsIcon from "../../assets/icons/settings-icon.svg";
+
 const Navbar = () => {
   return (
     <>
@@ -47,7 +66,43 @@ const Navbar = () => {
       </div>
       <div className="flex md:hidden bg-white px-5 py-1 items-center place-content-between">
         <div>
-          <img className="w-8" src={sidebar} />
+          <Drawer>
+            <DrawerTrigger>
+              <img className="w-8 inline-block " src={sidebar} />
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle></DrawerTitle>
+                <DrawerDescription></DrawerDescription>
+                <div className=" font-bold">
+                  <div className="flex mx-3 my-2 px-2 py-3 rounded-md cursor-default hover:bg-zinc-100">
+                    <img src={dashboardIcon} alt="sidebar-icon"></img>
+                    <p className="ml-2">Dashboard</p>
+                  </div>
+                  <div className="flex mx-3 my-2 px-2 py-3 cursor-default text-black bg-zinc-100 rounded-md">
+                    <img src={studentsIcon} alt="sidebar-icon"></img>
+                    <p className="ml-2">Students</p>
+                  </div>
+                  <div className="flex mx-3 my-2 px-2 py-3 rounded-md cursor-default hover:bg-zinc-100">
+                    <img src={chapterIcon} alt="sidebar-icon"></img>
+                    <p className="ml-2">Chapter</p>
+                  </div>
+                  <div className="flex mx-3 my-2 px-2 py-3 rounded-md cursor-default hover:bg-zinc-100">
+                    <img src={helpIcon} alt="sidebar-icon"></img>
+                    <p className="ml-2">Help</p>
+                  </div>
+                  <div className="flex mx-3 my-2 px-2 py-3 rounded-md cursor-default hover:bg-zinc-100">
+                    <img src={reportsIcon} alt="sidebar-icon"></img>
+                    <p className="ml-2">Reports</p>
+                  </div>
+                  <div className="flex mx-3 my-2 px-2 py-3 rounded-md cursor-default hover:bg-zinc-100">
+                    <img src={settingsIcon} alt="sidebar-icon"></img>
+                    <p className="ml-2">Settings</p>
+                  </div>
+                </div>
+              </DrawerHeader>
+            </DrawerContent>
+          </Drawer>
         </div>
         <div>
           <img className="h-12" src={logo} />
